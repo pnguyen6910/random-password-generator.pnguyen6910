e01dbd725fee07c329ba42eb0a321f769cb23e75
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-02-06 17:39:43 -0600

    created variables for letters and ids for elements on html

---
+++ .history/script_20210206173444.js
@@ -0,0 +1,5 @@
+let generate = document.querySelector("#generate")
+let error = document.querySelector("#error")
+let password = document.querySelector("#password")
+
+const cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
